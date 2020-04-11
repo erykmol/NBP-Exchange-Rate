f01c91b7f9ff5529fe55020f42d63349d20fb590
Eryk Mól <bagdad610@gmail.com>
2020-04-11 13:02:02 +0200

    Add CurrencyViewController, custom text field with date picker, and setup ui elements in storyboard

---
+++ NBP Exchange Rate/NBP Exchange Rate/Views/CurrencyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CurrencyViewController.swift
+//  NBP Exchange Rate
+//
+//  Created by Eryk Mól on 10/04/2020.
+//  Copyright © 2020 Eryk Mól. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ NBP Exchange Rate/NBP Exchange Rate/Views/DatePickerTextField.swift
@@ -0,0 +1,32 @@
+//
+//  NoCaretTextField.swift
+//  NBP Exchange Rate
+//
+//  Created by Eryk Mól on 10/04/2020.
+//  Copyright © 2020 Eryk Mól. All rights reserved.
+//
+
+import UIKit
+
+class NoCaretTextField: UITextField {
+    
+    let dateFormatter = DateFormatter()
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let datePicker = UIDatePicker()
+        datePicker.addTarget(self, action: #selector(onDateChange(_:)), for: .valueChanged)
+        datePicker.datePickerMode = .date
+        inputView = datePicker
+        text = dateFormatter.string(from: datePicker.date)
+    }
+    
+    override func caretRect(for position: UITextPosition) -> CGRect {
+        return .zero
+    }
+
+    @objc func onDateChange(_ sender: UIDatePicker) {
+        text = dateFormatter.string(from: sender.date)
+    }
+}
